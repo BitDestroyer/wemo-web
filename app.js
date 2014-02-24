b67cb479eac0a4a4b072adf46c0efad31eee6429
James Miller <jrmiller84@gmail.com>
2014-02-24 17:06:07 -0500

    Removing mongoose.

--- app.js
+++ app.js
@@ -1,30 +1,9 @@
 var app         = require('express')(),
-    mongoose    = require('mongoose'),
     fs          = require('fs'),
     config      = require('./config/config'),
     http        = require('http'),
     server      = http.createServer(app),
     wemo        = require('./wemo');
-
-mongoose.connect(config.db);
-
-var db = mongoose.connection;
-
-db.on('error', function () {
-
-    throw new Error('unable to connect to database at ' + config.db);
-
-});
-
-var modelsPath = __dirname + '/app/models';
-
-fs.readdirSync(modelsPath).forEach(function (file) {
-
-    if (file.indexOf('.js') >= 0) {
-        require(modelsPath + '/' + file);
-    } 
-
-});
 
 require('./config/express')(app, config);
 require('./config/routes')(app);
@@ -39,9 +18,6 @@ wemo.listen(server);
     //  wemoNode.sendCommand("socket_setbinarystate", object, {"binarystate": 1});
 
 }.bind(this));
-
-
-
 
 var turnOffWemoSwitch = function(object){
     wemoNode.sendCommand("socket_setbinarystate", object, {"binarystate": 0});
